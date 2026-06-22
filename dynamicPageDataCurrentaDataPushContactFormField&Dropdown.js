2c0f48726fd3240f6a1cb42616e20bd08299d0e8
ahmedraza069 <rakibulhasanraza@gmail.com>
2026-06-22 15:35:42 +0600

    everything working properly

--- dynamicPageDataCurrentaDataPushContactFormField&Dropdown.js
+++ dynamicPageDataCurrentaDataPushContactFormField&Dropdown.js
@@ -13,6 +13,7 @@ $w.onReady(function () {
         // Breadcrumb
         $w("#text933").text = "Home > Projects > " + projectTitle;
 
+        // Form fields
         setTimeout(() => {
             if (projectTitle && projectType) {
                 $w("#form1").setFieldValues({
@@ -20,10 +21,60 @@ $w.onReady(function () {
                     "dropdown_c537": projectType,
                 });
             }
-            console.log("Project Title:", projectTitle);
-            console.log("Project Type:", projectType);
         }, 1500);
 
     });
+
+    // Next / Previous Navigation
+    try {
+        $w('#dynamicDataset').onReady(async () => {
+            const currentItem = $w('#dynamicDataset').getCurrentItem();
+
+            if (!currentItem) return;
+
+      
+            // console.log("Current Item Keys:", Object.keys(currentItem));
+            // console.log("Current Item Full:", JSON.stringify(currentItem));
+
+            const results = await wixData.query('Services')
+                .ascending('title')
+                .limit(1000)
+                .find();
+
+            const allItems = results.items;
+            const totalItems = allItems.length;
+            const currentIndex = allItems.findIndex(item => item._id === currentItem._id);
+
+            if (totalItems <= 1) {
+                $w('#next').hide();
+                $w('#previous').hide();
+                return;
+            }
+
+            const nextIndex = (currentIndex + 1) % totalItems;
+            const nextItem = allItems[nextIndex];
+
+            const prevIndex = (currentIndex - 1 + totalItems) % totalItems;
+            const prevItem = allItems[prevIndex];
+
+     
+            // console.log("Next Item Keys:", Object.keys(nextItem));
+            // console.log("Next Item Full:", JSON.stringify(nextItem));
+
+            $w('#next').label = "Next";
+            $w('#next').onClick(() => {
+                wixLocation.to(`/project-details/${nextItem.slug || nextItem.title}`);
+            });
+
+            $w('#previous').label = "Previous";
+            $w('#previous').onClick(() => {
+                wixLocation.to(`/project-details/${prevItem.slug || prevItem.title}`);
+            });
+
+        });
+
+    } catch (error) {
+        console.error('Navigation Error:', error);
+    }
 
 });
